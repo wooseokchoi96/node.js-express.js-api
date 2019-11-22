7316658bc330018f668196ac01251dd14e790003
Andy Choi <wooseokchoi96@gmail.com>
2019-11-22 16:09:23 -0500

    Created dotenv file for db link and connected to db using mongoose

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+// Middleware
+app.use(express.json());
 
 // Routes
-
 app.get('/', (req, res) => res.send('You are on home'));
 
+// Connect to DB
+mongoose.connect(
+    process.env.DBCONNECT, 
+    { useNewUrlParser: true,
+      useUnifiedTopology: true, },
+    () => {
+        console.log('CONNECTED TO DB');
+    }
+);
 
 // Listen to server
-
 app.listen(3000);
